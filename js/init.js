5684b3ac6a3b1e61d6a163a912a2287a4421a771
Wilson Chen <36680838+KeroseneTrampolines@users.noreply.github.com>
2020-02-04 15:41:10 -0500

    Design Brief blocked out

--- js/init.js
+++ js/init.js
@@ -4,5 +4,10 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function () {
+  $(".dropdown-trigger").dropdown();
+});
